2e3123695fba3e2771565fc503650e2c8a374612
KWEYAMBAPETER <kweyamabapeter6@gmail.com>
2026-03-30 15:50:58 +0300

    Add a figure tree

---
+++ stickFig.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    
+    printf("                          Draw a figure
+                               |
+                 ______________|______________
+                |              |              |
+           Draw circle    Draw triangle   Draw intersecting
+                               |            lines
+                          _____|_____
+                         |           |
+                    Draw intersecting  Draw base
+                         lines
+        ");
+    return 0;
+}
